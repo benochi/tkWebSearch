a6fcfdd1c536c1848fcbd439b338b89d5a4b92e8
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-01-06 17:08:36 -0600

    Update search.py

--- search.py
+++ search.py
@@ -1,31 +1,51 @@
 import tkinter as tk
 import requests
+from bs4 import BeautifulSoup
+import csv
 
 # Create the main window
 window = tk.Tk()
-window.title("HTML Viewer")
+window.title("Shipping Manager Scraper")
 
-# Create the input field
+# Create the input fields
+url_label = tk.Label(window, text="Website URL:")
+url_label.pack()
+
 url_entry = tk.Entry(window)
 url_entry.pack()
 
-# Create the "Get HTML" button
-def get_html():
-    # Get the URL from the input field
+file_label = tk.Label(window, text="Save to File:")
+file_label.pack()
+
+file_entry = tk.Entry(window)
+file_entry.pack()
+
+# Create the "Scrape" button
+def scrape():
+    # Get the URL and file path from the input fields
     url = url_entry.get()
+    file_path = file_entry.get()
 
     # Send a request to the URL
     response = requests.get(url)
 
-    # Get the HTML data from the response
-    html = response.text
+    # Parse the HTML of the website
+    soup = BeautifulSoup(response.text, "html.parser")
 
-    # Display the HTML data
-    html_label = tk.Label(window, text=html)
-    html_label.pack()
+    # Extract the shipping manager information
+    shipping_managers = []
+    for div in soup.find_all("div", class_="shipping-manager"):
+        name = div.find("h3").text
+        email = div.find("span", class_="email").text
+        shipping_managers.append((name, email))
 
-get_html_button = tk.Button(window, text="Get HTML", command=get_html)
-get_html_button.pack()
+    # Write the shipping manager information to a CSV file
+    with open(file_path, "w", newline="") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerows(shipping_managers)
+
+scrape_button = tk.Button(window, text="Scrape", command=scrape)
+scrape_button.pack()
 
 # Run the main loop
 window.mainloop()
